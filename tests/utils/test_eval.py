d71afde80319c240440c3ce8ea61bb11715f01cb
Nicolas Pelzmann <nicolas.pelzmann@axpo.com>
2023-08-30 17:14:32 +0200

    updated cost function

--- tests/utils/test_eval.py
+++ tests/utils/test_eval.py
@@ -1,38 +1,35 @@
 import unittest
 
-from utils.eval import reward_function
+import numpy as np
+
+from utils.eval import reward_function, reward_sum
 
 class TestRewardFunction(unittest.TestCase):
-    def test_reward_function(self):
-        dV = -1.5
-        W_Q = 10
-        expected_reward = dV*W_Q*21.8
-        self.assertEqual(reward_function(dV, W_Q), expected_reward)
-        
-        dV = 0.5
-        W_Q = 10
-        expected_reward = dV*W_Q*3.0
-        self.assertEqual(reward_function(dV, W_Q), expected_reward)
-        
-        dV = -0.5
-        W_Q = 10
-        expected_reward = 30
-        self.assertEqual(reward_function(dV, W_Q), expected_reward)
-        
-        dV = -1.5
-        W_Q = -10
-        expected_reward = dV*W_Q*21.8
-        self.assertEqual(reward_function(dV, W_Q), expected_reward)
-        
-        dV = -0.5
-        W_Q = -10
-        expected_reward = dV*W_Q*3.0
-        self.assertEqual(reward_function(dV, W_Q), expected_reward)
-        
-        dV = 0.5
-        W_Q = -10
-        expected_reward = 30
-        self.assertEqual(reward_function(dV, W_Q), expected_reward)
+    def test_dV_m2_shunt_reactor_state(self):
+        dV = np.array([-3, -2, -1, 0])
+        shunt_reactor_state = np.array([True, True, True, True])
+        expected_reward = np.array([-24.4, 0, 3.0, 3.0])
+        self.assertTrue(np.allclose(reward_function(dV, shunt_reactor_state), expected_reward))
+    
+    def test_dV_p1_not_shunt_reactor_state(self):
+        dV = np.array([1, 2, 3, 4])
+        shunt_reactor_state = np.array([False, False, False, False])
+        expected_reward = np.array([3, 0, -24.4, -24.4])
+        self.assertTrue(np.allclose(reward_function(dV, shunt_reactor_state), expected_reward))
+
+class TestRewardSum(unittest.TestCase):
+    def test_no_weighing_factors(self):
+        dV = np.array([-3, -4, 0, 1])
+        shunt_reactor_state = np.array([True, True, False, False])
+        expected_reward_sum = -24.4 + -24.4*(5/6) + 3*(2/3) + 3*0.5
+        self.assertAlmostEqual(reward_sum(dV, shunt_reactor_state), expected_reward_sum)
+    
+    def test_weighing_factors(self):
+        dV = np.array([-3, -4, 0, 1])
+        shunt_reactor_state = np.array([True, True, False, False])
+        weighing_factors = np.array([1, 1, 0.5, 0.5])
+        expected_reward_sum = -24.4 + -24.4 + 3*0.5 + 3*0.5
+        self.assertAlmostEqual(reward_sum(dV, shunt_reactor_state, weighing_factors), expected_reward_sum)
         
 
 if __name__ == '__main__':
--- utils/eval.py
+++ utils/eval.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 import numpy as np
 
 
-def reward_function(dV, W_Q):
+def reward_function_old(dV: np.array, W_Q: np.array) -> float:
     """
     Calculates the financial reward for a substation from voltage and reactive energy (=reactive power for one hour).
     
@@ -10,12 +12,33 @@ def reward_function(dV, W_Q):
         W_Q: reactive energy [Mvarh]
         
     Returns:
-        financial reward
+        reward/penalty resulting from the given voltage deviations and reactive energy
     """
     c_mat = dV*np.sign(W_Q) > - 1
     nc_mat = dV*np.sign(W_Q) < -2
-    reward = 3*np.abs(W_Q)*c_mat - 21.8*np.abs(W_Q)*nc_mat
+    reward = 3*np.abs(W_Q)*c_mat - 24.4*np.abs(W_Q)*nc_mat
     return reward
 
-def reward_function_simplified(dV):
-    pass
+def reward_function(
+        dV: np.array, 
+        shunt_reactor_state: np.array,
+        ) -> np.array:
+    
+    dV_m2 = dV < -2.
+    dV_m1 = dV >= -1.
+    dV_p2 = dV > 2. 
+    dV_p1 = dV <= 1.
+    reward = (-24.4*dV_m2 + 3*dV_m1) * shunt_reactor_state \
+           + (-24.4*dV_p2 + 3*dV_p1) * ~shunt_reactor_state
+           
+    return reward
+
+def reward_sum(
+        dV: np.array, 
+        shunt_reactor_state: np.array,
+        weighing_factors: Optional[np.array] = None,
+        ) -> float:
+    if weighing_factors is None:
+        weighing_factors = np.linspace(1, 0.5, len(dV))
+    
+    return np.sum(reward_function(dV, shunt_reactor_state) * weighing_factors)
