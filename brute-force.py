7d463dd6cc511c32a2a910d5429f4e3de2f2ad7c
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2023-09-16 15:21:46 +0200

    Update brute-force.py

--- brute-force.py
+++ brute-force.py
@@ -8,17 +8,28 @@ def num_flips(given):
         flips += abs(given[i] - given[i-1])
     return flips
 
-def best_permutation(dV, shunt_states, reward_sum, best_array):
+def best_permutation((dV, WQ, sensitivity), (shunt_states, shunt_statesinv), reward_sum, best_array):
     replacement_values = [0.35, 0.90]
     one_indices = [i for i, val in enumerate(row) if val == 1]
     replacement_combinations = list(itertools.product(replacement_values, repeat=len(one_indices)))
+
+    dV_off, dV_min, dV_max = eval.virtual_voltages(dV, WQ, sensitivity)
 
     for combo in replacement_combinations:
         modified_array = original_array[:]
         for i, val in zip(one_indices, combo):
             modified_array[i] = val
 
-        score = eval.reward_sum(dV, shunt_states, modified_array)
+        DVk = []
+        for k in modified_array:
+            if k == 0.90:
+                DVk += dV_max*k
+            elif k == 0.30:
+                DVk += dV_min*k
+            else:
+                DVk += dV_off*k
+        
+        score = eval.reward_sum(DVk, shunt_states, modified_array)
         if(score > reward_sum):
             reward_sum = score
             best_array = modified_array
@@ -37,12 +48,13 @@ def simulate_switch(dV, WQ, sensitivity):
         S_ktemp = np.fliplr(np.tril(np.ones((96-k, 96-k)), -1))
         S_k = np.hstack((np.ones([S_ktemp.shape[0], k], S_ktemp.dtype)))
         S_kinv = np.subtract(np.ones(S_k.shape), S_k)
-    
-        dV_off, dV_min, dV_max = eval.virtual_voltages(dV, WQ, sensitivity)
-        DVk = np.add(np.multiply(S_k, dV_on), np.multiply(S_kinv, dV_off))
         
         for i in range(DVk.shape[0]):
-            reward_sum_temp, best_array_temp = best_permutation(DVk[i], S_k[i], reward_sum, best_array)
+            reward_sum_temp, best_array_temp = best_permutation((dV, WQ, sensitivity), 
+                                                                (S_k[i], S_kinv[i]), 
+                                                                reward_sum, 
+                                                                best_array
+                                                               )
             if reward_sum_temp > reward_sum:
                 reward_sum = reward_sum_temp
                 best_array = best_array_temp
